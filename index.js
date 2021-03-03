fb4d02673728f12fcf98a24a8b980bae3a78b2f5
Antony Fourchault <antonyfourchault@gmail.com>
2021-03-03 12:42:11 +0100

    cpu + ram

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+let osu = require('node-os-utils')
+
 function infoStart(port, name) {
     console.log(`${name} listening at http://localhost:${port}`)
 }
 
-let osu = require('node-os-utils')
-let cpu = osu.cpu
-let mem = osu.mem
-cpu.usage()
-    .then(info => {
-        console.log('CPU usage: ' + info + ' %')
-    })
-mem.used()
-    .then(info => {
-        console.log('RAM usage: ' + (info.usedMemMb * 100 / info.totalMemMb).toFixed(2) + ' %')
-    })
+function usage() {
+    let cpu = osu.cpu
+    let mem = osu.mem
+    cpu.usage()
+        .then(info => {
+            console.log('CPU usage: ' + info + ' %')
+        })
+    mem.used()
+        .then(info => {
+            console.log('RAM usage: ' + (info.usedMemMb * 100 / info.totalMemMb).toFixed(2) + ' %')
+        })
+}
 
-module.exports = {infoStart}
+module.exports = {infoStart, usage}
